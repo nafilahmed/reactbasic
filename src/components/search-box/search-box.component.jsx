20bd0d934b9e55a13f2a0cb365016ae70b14fa20
Nafil Ahmed <nafil.ahmed@arpatech.com>
2024-05-24 22:42:56 +0500

    Class Component App Finished

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import './search-box.styles.css';
+
+class SearchBox extends Component {
+
+	render(){
+
+		return (
+
+			<div>
+				<input className='search-box' type='search' placeholder='search monsters' 
+		          onChange={this.props.onChangeHandler}
+		        />
+			</div>
+		)
+	}
+}
+
+export default SearchBox;
